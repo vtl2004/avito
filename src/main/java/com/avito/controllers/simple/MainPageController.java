5b5e1b81c74e80f866d8eac75d1f71f841c0b420
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-17 17:38:18 +0300

    uses  @GetMapping

--- src/main/java/com/avito/controllers/simple/MainPageController.java
+++ src/main/java/com/avito/controllers/simple/MainPageController.java
@@ -1,12 +1,12 @@
 package com.avito.controllers.simple;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String getMainPage() {
         return "index";
     }
