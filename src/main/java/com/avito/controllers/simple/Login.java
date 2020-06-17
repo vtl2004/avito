e9e8cf5233abc372410f5ea2d28ff52ffb860b40
aleks <aleksandr.law@gmail.com>
2020-06-17 10:01:43 +0300

    login controller  170620 1001

---
+++ src/main/java/com/avito/controllers/simple/Login.java
@@ -0,0 +1,13 @@
+package com.avito.controllers.simple;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Login {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
