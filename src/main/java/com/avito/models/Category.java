80b0434c9f93b1ea0403532bcf1efc05e5c6be8c
tima <tima.ogurtzova@yandex.ru>
2020-06-19 19:05:34 +0300

    id id long -> Long

--- src/main/java/com/avito/models/Category.java
+++ src/main/java/com/avito/models/Category.java
@@ -13,7 +13,7 @@ public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
