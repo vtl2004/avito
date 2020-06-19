011ed74aa4bf67bc511268738517addb0307507c
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-19 12:44:54 +0300

    deletes @Getter and @Setter annotations, adds @Data

--- src/main/java/com/avito/models/Posting.java
+++ src/main/java/com/avito/models/Posting.java
@@ -1,10 +1,10 @@
 package com.avito.models;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "posting")
 public class Posting {
@@ -12,36 +12,22 @@ public class Posting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "posting_id")
-    @Getter
-    @Setter
     private Long id;
 
-    @Getter
-    @Setter
     private String title;
 
-    @Getter
-    @Setter
     @Column(name = "short_description")
     private String shortDescription;
 
-    @Getter
-    @Setter
     @Column(name = "full_description")
     private String fullDescription;
 
-    @Getter
-    @Setter
     @Column(name = "user_id")
     private Long userId;
 
-    @Getter
-    @Setter
     @Column(name = "category_id")
     private Long categoryId;
 
-    @Getter
-    @Setter
     @Column(name = "image_path")
     private String imagePath;
 
