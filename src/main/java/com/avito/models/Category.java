653cb316c515633d8addaaab60f881067e6bd117
Boris Muzychuk <muzychuk.boris@gmail.com>
2020-06-21 17:21:48 +0300

    deletes id from constructor

--- src/main/java/com/avito/models/Category.java
+++ src/main/java/com/avito/models/Category.java
@@ -24,8 +24,7 @@ public class Category {
     public Category() {
     }
 
-    public Category(long id, String name, Set<Category> subCategories) {
-        this.id = id;
+    public Category(String name, Set<Category> subCategories) {
         this.name = name;
         this.subCategories = subCategories;
     }
