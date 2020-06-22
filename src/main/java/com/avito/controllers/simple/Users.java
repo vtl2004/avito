c08278930617a955fe79a050d314e7449f7724a0
jimmim <kalinin_av@list.ru>
2020-06-22 11:30:57 +0300

    add users controller add bootstrap

--- src/main/java/com/avito/controllers/simple/Users.java
+++ src/main/java/com/avito/controllers/simple/Users.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Login {
+public class Users {
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
+    @GetMapping("/users")
+    public String users() { return "users"; }
 
 }
