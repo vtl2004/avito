c5266ec6e81f22a0f74667873ddae7a3f83a4c6b
Vadim Karpov <ksvkvv1990@gmail.com>
2020-06-22 16:58:32 +0300

    test

---
+++ src/main/java/com/avito/configs/security/handler/LoginSuccessHandler.java
@@ -0,0 +1,43 @@
+package com.avito.configs.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+@Component
+public class LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
+                                        HttpServletResponse httpServletResponse,
+                                        Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.sendRedirect(determineTargetUrl(authentication));
+    }
+    protected String determineTargetUrl(Authentication authentication) {
+        boolean isUser = false;
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
+                isUser = true;
+                break;
+            }
+
+        }
+
+        if (isUser) {
+            return "/";
+
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+}
